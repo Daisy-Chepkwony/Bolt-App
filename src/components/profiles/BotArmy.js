c5099fdb122208b844516f8c4de3e0908626463a
+ <+>
2023-01-23 23:48:28 +0300

    bolt app

--- src/components/profiles/BotArmy.js
+++ src/components/profiles/BotArmy.js
@@ -1,12 +1,13 @@
 
 
-const BotArmy = ({item, handleOnClick})=> {
+
+const BotArmy = (item, handleOnClick)=> {
     return(
-        <div onClick={()=> handleOnClick(item.id)} className="card col-2 p-0 m-2">
-                <img  className="card-img-top" alt="..."/>
+        <div  key={item.id} onClick={()=> handleOnClick(item.id)} className="card col-2 p-0 m-2">
+                <img src={item.avatar_url} className="card-img-top" alt="..."/>
                 <div className="card-body">
-                    <h5 className="card-title"></h5>
-                    <p className="card-text"></p>
+                    <h5 className="card-title">{item.catchphrase}</h5>
+                    <p className="card-text">{item.bot_class}</p>
                     {/* <p className="btn btn-sm text-small btn-primary btn-block m-2">View Item Details</p> */}
                     <button className="btn btn-sm text-small btn-danger btn-block">Delete Items</button>
 
@@ -15,4 +16,4 @@ const BotArmy = ({item, handleOnClick})=> {
     )
 }
 
-export default BotArmy 
+export default BotArmy
--- src/components/profiles/BotCollection.js
+++ src/components/profiles/BotCollection.js
@@ -1,1 +1,62 @@
+import {useState, useEffect} from "react"
+import BotArmy from "./BotArmy"
+
+
+const BotCollection = ()=> {
+    const [items, setItems] = useState([])
+    const [botItems, setBotItems] = useState([])
+    
+
+    useEffect(()=>{
+        fetch("http://localhost:3000/bots")
+        .then((r)=>r.json())
+        .then((data)=> setItems(data))
+    }, [])
+
+
+    const addItemBot = (id)=> {
+        const item= items.find(item => item.id === id)
+        const bot = botItems.find(item => item.id === id)
+        bot? alert("existing") :  setBotItems([...botItems, item])
+    }
+
+    const removeItemBot = (id)=> {
+        const newBotItems = botItems.filter((item) => item.id !== id)
+        setBotItems(newBotItems)
+    }
+
+
+    return(
+        <div className="row">
+            <div className="col-12 mx-auto border p-4">
+                <div className="row">
+                    {
+                        botItems.map((item)=> {
+                            return (
+                                <BotArmy  key={item.id} item={item} handleOnClick={removeItemBot}/>
+                            )
+                        })
+                    }
+                    </div>
+            </div>
+            <div className="col-12 mx-auto border p-4">
+                <div className="row">
+                {
+                    items.map((item)=> {
+                        return (
+                            <BotArmy key={item.id} item={item} handleOnClick={addItemBot}/>
+                        )
+                    })
+                }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default BotCollection
+
+
+
+
 
